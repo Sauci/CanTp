878422514c8f8ca5b86f03c6fcd43fd0fa90b8ce
Sauci <guillaumesottas@gmail.com>
2019-08-23 09:34:14 +0200

    set line ending to CRLF instead of CR.

--- test/stub/CanIf_Types.h
+++ test/stub/CanIf_Types.h
@@ -1,1 +1,26 @@
-/** * @file CanIf_Types.h * @author Guillaume Sottas * @date 07/12/2017 */#ifndef CANIF_TYPES_H#define CANIF_TYPES_H#ifdef __cplusplusextern "C"{#endif /* ifdef __cplusplus */#include "Std_Types.h"#include "ComStack_Types.h"#ifdef __cplusplus}#endif /* ifdef __cplusplus */#endif /* define CANIF_TYPES_H */
+/**
+ * @file CanIf_Types.h
+ * @author Guillaume Sottas
+ * @date 07/12/2017
+ */
+
+#ifndef CANIF_TYPES_H
+#define CANIF_TYPES_H
+
+#ifdef __cplusplus
+
+extern "C"
+{
+
+#endif /* ifdef __cplusplus */
+
+#include "Std_Types.h"
+#include "ComStack_Types.h"
+
+#ifdef __cplusplus
+
+}
+
+#endif /* ifdef __cplusplus */
+
+#endif /* define CANIF_TYPES_H */
